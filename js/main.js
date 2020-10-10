ad31ecdf8923a7924bf60c6721c03033be304151
Sergey Murzak <murzak301@gmail.com>
2020-10-11 01:44:05 +0600

    Додела бургер меню,и адаптив

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 let burgerBtn = document.querySelector('.burger__item');
+let nav = document.querySelector('.nav');
+let navLink = document.querySelectorAll('.nav__link');
+
 burgerBtn.addEventListener('click',openMenu);
 function openMenu(){
     burgerBtn.classList.toggle('active');
+    nav.classList.toggle('active');
+    navLink.forEach(function(event){
+        event.classList.add('active')
+    })
 }
