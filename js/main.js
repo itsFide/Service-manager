cc37f8f73eaa82fc7fb463d357e0edfa39b9986f
Sergey Murzak <murzak301@gmail.com>
2020-10-11 14:32:36 +0600

    Сделал адаптив первой секции

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 let burgerBtn = document.querySelector('.burger__item');
 let nav = document.querySelector('.nav');
 let navLink = document.querySelectorAll('.nav__link');
+let introInner = document.querySelector('.intro__inner');
+let singUp = document.querySelector('.sing__up')
 
+window.addEventListener('load',loadText)
+function loadText(){
+    introInner.style.opacity = "1";
+    
+}
+setTimeout(loadText,2000)
 burgerBtn.addEventListener('click',openMenu);
 function openMenu(){
+    
     burgerBtn.classList.toggle('active');
     nav.classList.toggle('active');
     navLink.forEach(function(event){
         event.classList.add('active')
     })
+    if(nav.classList.contains('active')){
+    nav.insertAdjacentHTML('beforeend','<a href="" class="sing__up sing__up-burger" >sing up</a>');
+    
+    } else if(!nav.classList.contains('active')){
+        nav.removeChild(document.querySelector('.sing__up-burger'))
+    }
 }
+
